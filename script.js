545860990f270f0c2315175ff54b2be5ae56f3bc
Anirban Roy <156198466+AnirbanRoy169@users.noreply.github.com>
2024-02-13 02:21:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#element1', {
+    strings: ["You know It's not just a kiss"],
+    typeSpeed: 50,
+    showCursor: false,
+});
+var typed = new Typed('#element2', {
+    strings: ["It's the feel of your soul ❤️"],
+    typeSpeed: 50,
+    showCursor: false,
+    startDelay: 3500,
+});
